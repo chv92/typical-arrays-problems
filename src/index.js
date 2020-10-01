9e08124e6843051b1be855b928d664b7a1d8be57
chv92 <vitalii.ch92@gmail.com>
2020-10-01 19:14:30 +0200

    feat: solve Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (Array.isArray(array) && array.length) {
+    let minValue = Math.min(...array);
+    return minValue;
+  } else {
+    return 0;
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (Array.isArray(array) && array.length) {
+    let maxValue = Math.max(...array);
+    return maxValue;
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let i = 0;
+  let sum = 0;
+  if (Array.isArray(array) && array.length) {
+    while (i < array.length) {
+      sum = sum + (array[i]);
+      i++;
+    }
+    let avg = sum / array.length;
+    return avg;
+  } else {
+    return 0;
+  }
 }
